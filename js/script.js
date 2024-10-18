416f05b3631bf0a2cd65923506aa369bc7a7c46e
DiAlves96 <145405314+DiAlves96@users.noreply.github.com>
2024-10-18 01:15:48 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 
-form.addEventListener ('submit', function(event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const weight = document.getElementById('weigth').value;
+    const weight = document.getElementById('weight').value;
     const height = document.getElementById('height').value;
 
     const bmi = (weight / (height * height)).toFixed(2);
@@ -11,5 +11,26 @@ form.addEventListener ('submit', function(event) {
     const value = document.getElementById('value');
     let description = '';
 
+    value.classList.add('attention');
+
     document.getElementById('infos').classList.remove('hidden');
-});
+
+    if (bmi < 18.5) {
+        description = 'Você está abaixo do peso!'
+    } else if (bmi >= 18.5 && bmi <=25) {
+        description = "Você está no peso ideal!";
+        value.classList.remove('attention');
+        value.classList.add('normal');
+    } else if (bmi > 25 && bmi <=30) {
+        description = "Você está está com sobrepeso!";
+    } else if (bmi >30 && bmi <=35) {
+        description = "Você está com obesidade moderada!";
+    } else if (bmi >35 && bmi <=40) {
+        description = "Você está com obesidade severa!";
+    } else  {
+        description = "Você está com obesidade morbida!";
+    }
+    
+    value.textContent = bmi.replace('.',',');
+    document.getElementById('description').textContent = description;
+})
